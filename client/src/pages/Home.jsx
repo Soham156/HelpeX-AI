6ccf1e1a0c39c513482efa28bb0317b9e0a905c4
Soham156 <sohamchaudhari165@gmail.com>
2025-07-29 03:03:42 +0530

    homepage error fix

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Navbar from './../components/Navbar';
 import Hero from '../components/Hero';
-import AITools from '../components/AITools';
+import AiTools from '../components/AiTools';
 import Testimonial from '../components/Testimonial';
 import Plan from '../components/Plan';
 import Footer from '../components/Footer';
@@ -9,12 +9,12 @@ import Footer from '../components/Footer';
 const Home = () => {
   return (
     <>
-        <Navbar />
-        <Hero />
-        <AITools />
-        <Testimonial />
-        <Plan />
-        <Footer />
+      <Navbar />
+      <Hero />
+      <AiTools />
+      <Testimonial />
+      <Plan />
+      <Footer />
     </>
   )
 }
